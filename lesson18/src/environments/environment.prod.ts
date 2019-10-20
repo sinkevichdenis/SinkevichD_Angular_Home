60c23cc9b70ce861daa0abd67305db7106b63dd3
SinkevichDenis <rggrodno@gmail.com>
2019-10-20 03:17:14 +0300

    fix(lesson18): fix some bugs

--- lesson18/src/environments/environment.prod.ts
+++ lesson18/src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyCH4DP1W4R9Q28d4__sWTVNG_IzLYW8vak",
+    authDomain: "lesson18-577ce.firebaseapp.com",
+    databaseURL: "https://lesson18-577ce.firebaseio.com",
+    projectId: "lesson18-577ce",
+    storageBucket: "lesson18-577ce.appspot.com",
+    messagingSenderId: "174029948357",
+    appId: "1:174029948357:web:7f405309323b1c0e4437ac",
+    measurementId: "G-69YR21Y8YX"
+  }
 };
