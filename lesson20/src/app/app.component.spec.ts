6f38d2de10f16e7dd5e48e401cb7b5e4bf1ca684
SinkevichDenis <rggrodno@gmail.com>
2019-10-30 00:42:28 +0300

    feat(lesson20): finish homework

--- lesson20/src/app/app.component.spec.ts
+++ lesson20/src/app/app.component.spec.ts
@@ -4,21 +4,31 @@ import { TestBed, async } from '@angular/core/testing';
 import { AppComponent } from './app.component';
 
 describe('App: CompleteGuideFinalWebpack', () => {
+  let fixture;
+  let app;
+
   beforeEach(() => {
     TestBed.configureTestingModule({
       declarations: [
         AppComponent
       ],
     });
+
+    fixture = TestBed.createComponent(AppComponent);
+    app = fixture.debugElement.componentInstance;
   });
 
   it('should create the app', async(() => {
-    let fixture = TestBed.createComponent(AppComponent);
-    let app = fixture.debugElement.componentInstance;
     expect(app).toBeTruthy();
   }));
 
-  it(`should have as title 'app works!'`, async(() => {}));
+  it(`should have as title 'app works!'`, async(() => {
+    expect(app.title).toEqual('app works!');
+  }));
 
-  it('should render title in a h1 tag', async(() => {}));
+  it('should render title in a h1 tag', async(() => {
+    fixture.detectChanges();
+    const compiled = fixture.debugElement.nativeElement;
+    expect(compiled.querySelector('h1').textContent).toContain('app works!');
+  }));
 });
--- lesson20/src/app/user/user.component.spec.ts
+++ lesson20/src/app/user/user.component.spec.ts
@@ -5,13 +5,19 @@ import { UserComponent } from './user.component';
 import { UserService } from "./user.service";
 import { DataService } from "../shared/data.service";
 import {By} from "@angular/platform-browser";
-import {inject} from "@angular/core";
-
-const mockUser = {name: 'Name'};
 
 class MockService {
-  getUser() {
-    return mockUser.name;
+  user = {
+    name: 'Name'
+  };
+
+  getDetails() {
+    const resultPromise = new Promise((resolve, reject) => {
+      setTimeout(() => {
+        resolve('fake data');
+      }, 1500);
+    });
+    return resultPromise;
   }
 }
 
@@ -25,14 +31,17 @@ describe('Component: User', () => {
     const bed = TestBed.configureTestingModule({
       declarations: [UserComponent],
       providers: [
-        DataService,
-        { provide: UserService, useClass: MockService }
+        {provide: DataService, useValue: MockService},
+        {provide: UserService, useValue: MockService}
       ]
     });
 
+// я не понимаю, почему не переключается на MockService при прохождении тестов
+
     fixture = TestBed.createComponent(UserComponent);
     app = fixture.debugElement.componentInstance;
-    userService = bed.get(UserService);
+    userService = fixture.debugElement.injector.get(UserService);
+    dataService = fixture.debugElement.injector.get(DataService);
   });
 
   it('should create the app', () => {
@@ -40,35 +49,51 @@ describe('Component: User', () => {
   });
 
   it('should use the user name from the service', () => {
-
+    fixture.detectChanges();
+    expect(userService.user).toEqual(app.user);
   });
 
   it('should display the user name if user is logged in', () => {
-    const el = fixture.debugElement;
+    const el = fixture.debugElement.nativeElement;
     app.isLoggedIn = true;
     fixture.detectChanges();
-    expect(el.query(By.css('p')).nativeElement.textContent).toContain('User');
+    expect(el.querySelector('p').textContent).toContain('User');
   });
 
   it('shouldn\'t display the user name if user is not logged in', () => {
-    const el = fixture.debugElement;
+    const el = fixture.debugElement.nativeElement;
     app.isLoggedIn = false;
     fixture.detectChanges();
-    expect(el.query(By.css('p')).nativeElement.textContent).toBe('Please log in first');
+    expect(el.querySelector('p').textContent).toBe('Please log in first');
   });
 
-  it('shouldn\'t fetch data successfully if not called asynchronously', () => {});
+  it('shouldn\'t fetch data successfully if not called asynchronously', () => {
+    let spy = spyOn(dataService, "getDetails").and.returnValue(
+      Promise.resolve('fake data')
+    );
+    fixture.detectChanges();
+    expect(spy).toHaveBeenCalled();
+    expect(app.data).toBeUndefined();
+  });
 
   it('should fetch data successfully if called asynchronously', async(() => {
     // by using whenStable()
+    spyOn(dataService, "getDetails").and.returnValue(
+      Promise.resolve('fake data')
+    );
+    fixture.detectChanges();
+    fixture.whenStable().then(() => {
+      expect(app.data).toEqual('fake data');
+    });
   }));
 
-  it('should fetch data successfully if called asynchronously', fakeAsync((dataService: DataService) => {
-
-    dataService.getDetails().then(result => {
-      expect(result).toBe('Data');
-    });
-    tick(5000);
-
+  it('should fetch data successfully if called asynchronously', fakeAsync(() => {
+    // by using tick()
+    spyOn(dataService, "getDetails").and.returnValue(
+      Promise.resolve('fake data')
+    );
+    fixture.detectChanges();
+    tick(500);
+    expect(app.data).toEqual('fake data');
   }));
 });
